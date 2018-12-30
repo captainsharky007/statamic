9d9d38bc9e7184f5ebe466d7cd782e5da528cbbf
captainsharky007 <jevgenijs@zaremba.lv>
2018-12-30 20:59:54 +0200

    event form

---
+++ site/lang/en/form.php
@@ -0,0 +1,23 @@
+<?php
+return [
+  
+    // Labels
+    'subject_label' => 'Subject',
+    'event_label' => 'Event name',
+    'full_name_label' => 'Full name',
+    'company_name_label' => 'Company name',
+    'email_label' => 'Email',
+
+    // Errors
+    'required' => 'This field is required',
+    'full_name_error' => 'Invalid name',
+    'company_name_error' => 'Invalid name',
+    'email_error' => 'Invalid email',
+
+    // placeholder
+    'event_subject_placeholder' => 'Looking forward to meet at ',
+
+    // Events - book meeting form
+    'book_meeting_form_title' => 'Book meeting with us',
+    'book_meeting_form_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+];
---
+++ site/lang/lv/form.php
@@ -0,0 +1,23 @@
+<?php
+return [
+
+    // Labels
+    'subject_label' => 'Tēma',
+    'event_label' => 'Pasākum nosaukums',
+    'full_name_label' => 'Pilns vārds',
+    'company_name_label' => 'Uzņēmuma nosaukums',
+    'email_label' => 'E-pasts',
+
+    // Errors
+    'required' => 'This field is required',
+    'full_name_error' => 'Invalid name',
+    'company_name_error' => 'Invalid name',
+    'email_error' => 'Invalid email',
+
+    // placeholder
+    'event_subject_placeholder' => 'Looking forward to meet at ',
+
+    // Events - book meeting form
+    'book_meeting_form_title' => 'Book meeting with us',
+    'book_meeting_form_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+];
